411e41fe98fe5ca1bc3f558a69fe0aa61aae3dfe
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2021-07-10 11:16:59 -0300

    feat: Finish the BoardGame objects

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,10 +1,15 @@
+import './style.css'
 import CardGame from "../../components/CardGame";
 
 function BoardGame(amountGame) {
 	const $htmlCardGame = CardGame();
 	const $htmlBoardGame = $htmlCardGame.repeat(amountGame);
 
-	return $htmlBoardGame;
+	return `
+		<section class="board-game">
+			${$htmlBoardGame}
+		</section>
+	`;
 }
 
 export default BoardGame;
