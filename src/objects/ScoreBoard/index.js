bc77238dac2a7e96a11b0218ca6f98ad98fb0333
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2021-07-10 13:10:55 -0300

    ui: create ScoreBorad object

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,10 @@
+import PlayerName from '../../components/PlayerName'
+
+export default function ScoreBoard() {
+    return /*html*/ `
+        <header class="score-board">
+            ${PlayerName('player 1')}
+            ${PlayerName('player 2')}
+        </header>
+    `
+}
