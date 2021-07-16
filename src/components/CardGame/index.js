85eab2a58ad35997ee147ef130e28aec133eda86
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2021-07-16 17:46:57 -0300

    refactor: Rename class in CardGame

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 export default function CardGame(icon = "alura", alt = "loo da alura") {
     return `
-        <article class="card-start">
+        <article class="card-game">
                 <img src="./img/${icon}.svg" alt="${alt}"/>
         </article>
     `;
